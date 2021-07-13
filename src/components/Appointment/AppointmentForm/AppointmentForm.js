e847702b42f1b4528be192c4283ea49ce28f2d92
Shuvo Das <shuvo0381@gmail.com>
2021-07-14 04:32:37 +0600

    modal design doen

---
+++ src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -0,0 +1,121 @@
+import React from "react";
+import Modal from "react-modal";
+import { useForm } from "react-hook-form";
+import {connect} from 'react-redux'
+const customStyles = {
+  content: {
+    // width: "550px",
+    padding: "30px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
+};
+Modal.setAppElement("#root");
+
+const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn,date }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("submitted");
+  };
+
+  return (
+    <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+          <h3 className='text-center text-info'>{appointmentOn}</h3>
+          <p className='text-center text-secondary'><small>On {date.toDateString()}</small></p>
+        <form className='p-5' onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-group">
+            <input
+              name="name"
+              className="form-control"
+              placeholder="Enter Your Name"
+              {...register("name", { required: true })}
+            />
+            {errors.name && <span>This field is required</span>}
+          </div>
+          <div className="form-group my-3">
+            <input
+              name="phoneNumber"
+              className="form-control"
+              placeholder="Enter Your Phone Number"
+              {...register("phoneNumber", { required: true })}
+            />
+            {errors.phoneNumber && <span>This field is required</span>}
+          </div>
+          <div className="form-group">
+            <input
+              name="email"
+              className="form-control"
+              placeholder="Enter Your email"
+              {...register("email", { required: true })}
+            />
+            {errors.email && <span>This field is required</span>}
+          </div>
+          <div className="form-row row my-3">
+            <div className="form-group col-md-4">
+              <select
+                className="custom-select mr-sm-2"
+                name="gender"
+                className="form-control"
+                id="inlineFormCustomSelect"
+                {...register("gender", { required: true })}
+              >
+                <option selected value="Male">
+                  Male
+                </option>
+                <option value="Female">Female</option>
+                <option value="Others">Others</option>
+              </select>
+              {errors.gender && <span>This field is required</span>}
+            </div>
+            <div className="form-group col-md-4">
+              <input
+                type="text"
+                placeholder="Age"
+                name="age"
+                className="form-control"
+                {...register("age", { required: true })}
+              />
+              {errors.age && <span>This field is required</span>}
+            </div>
+            <div className="form-group col-md-4">
+              <input
+                type="text"
+                placeholder="Weight"
+                name="weight"
+                className="form-control"
+                {...register("weight", { required: true })}
+              />
+              {errors.weight && <span>This field is required</span>}
+            </div>
+          </div>
+          <div className="form-group ">
+            <input type="submit" className="btn btn-info w-100 text-white text-uppercase"/>
+          </div>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) =>{
+    return{
+        date: state.appointmentDate.date
+    }
+}
+
+export default connect(mapStateToProps,null) (AppointmentForm);
--- src/components/Appointment/BookingCard/BookingCard.js
+++ src/components/Appointment/BookingCard/BookingCard.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
+import ReactDOM from 'react-dom';
+import AppointmentForm from "../AppointmentForm/AppointmentForm";
 import './BookingCard.css'
+
+
 const BookingCard = ({booking}) => {
+  
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  // function afterOpenModal() {
+  //   // references are now sync'd and can be accessed.
+  //   subtitle.style.color = #f00;
+  // }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="col-md-4 mb-3">
       <div class="card text-center appointment-card" style={{ width: "18rem" }}>
@@ -8,7 +28,8 @@ const BookingCard = ({booking}) => {
           <h5 class="card-title text-info">{booking.subject}</h5>
           <h6 class="card-subtitle mb-2">{booking.visitingHour}</h6>
           <small class="card-text"> {booking.totalSpace} SPACES AVAILABLE </small>
-          <button className='btn btn-info text-white mt-2' style={{borderRadius: '0'}}>BOOK APPOINTMENT</button>
+          <button onClick={openModal} className='btn btn-info text-white mt-2' style={{borderRadius: '0'}}>BOOK APPOINTMENT</button>
+          <AppointmentForm modalIsOpen={modalIsOpen} closeModal={closeModal} appointmentOn={booking.subject}></AppointmentForm>
         </div>
       </div>
     </div>
